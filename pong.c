3d6f4c9f8e6d4d6e2e32b92ee7d1d770f9337d5a
SID <Siddharthkumar9c@gmail.com>
2026-01-03 23:34:41 +0530

    making a simple working model

--- pong.c
+++ pong.c
@@ -4,8 +4,8 @@
 #include <SDL3/SDL.h>
 
 // macros
-#define WINDOW_HEIGHT 500
-#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 640
+#define WINDOW_WIDTH 720
 
 // entity
 
@@ -14,6 +14,12 @@ typedef struct {
     float dx, dy;
     float height, width;
 } Entity;
+
+// paddle type
+typedef enum {
+    PADDLE_LEFT,
+    PADDLE_RIGHT
+} PaddleSide;
 
 // ball generator
 Entity ball_generator()
@@ -28,32 +34,40 @@ Entity ball_generator()
     return ball;
 }
 
-void ball_update( Entity *ball);
-void keyboard_input (Entity *paddle);
+void ball_update( Entity *ball ,bool left_paddle, bool right_paddle);
+void keyboard_input (Entity *paddle1 , Entity *paddle2);
+bool check_collision(Entity *ball, Entity *paddle);
 
 // renderer
 SDL_Renderer* renderer = NULL;
 
 //ball
-void draw(const Entity ball, const Entity paddle)
+void draw(const Entity ball, const Entity paddle1, const Entity paddle2)
 {
     SDL_FRect rect1 = {ball.x, ball.y, ball.width, ball.height};
-    SDL_FRect rect2 = {paddle.x, paddle.y, paddle.width, paddle.height};
+    SDL_FRect rect2 = {paddle1.x, paddle1.y, paddle1.width, paddle1.height};
+    SDL_FRect rect3 = {paddle2.x, paddle2.y, paddle2.width, paddle2.height};
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderFillRect(renderer, &rect1);
     SDL_RenderFillRect(renderer, &rect2);
+    SDL_RenderFillRect(renderer, &rect3);
 }
 
 //paddle
-Entity paddle_generator()
+Entity paddle_generator(PaddleSide const side)
 {
     Entity paddle;
-    paddle.x = 320;
-    paddle.y = 240;
+    paddle.x = 10.00;
+    paddle.y = WINDOW_HEIGHT/2;
     paddle.dx = 0.0f;
-    paddle.dy = 0.0f;
-    paddle.height = 29.00f;
+    paddle.dy = 4.0f;
+    paddle.height = 45.00f;
     paddle.width = 8.00f;
+
+    if (side == PADDLE_RIGHT)
+    {
+        paddle.x = WINDOW_WIDTH - paddle.width;
+    }
     return paddle;
 }
 
@@ -62,11 +76,11 @@ Entity paddle_generator()
 int main()
 {
 
-    if (!SDL_Init(SDL_INIT_VIDEO) != false) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("SDL_Init failed: %s", SDL_GetError());
         return 1;
     }
-    SDL_Window *window = SDL_CreateWindow("Pong Game", WINDOW_HEIGHT, WINDOW_WIDTH, SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS);
+    SDL_Window *window = SDL_CreateWindow("Pong Game", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS);
     if (window == NULL) {
         SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
         SDL_Quit();
@@ -84,7 +98,8 @@ int main()
     Uint8 r, g, b, a;
     SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a);
     Entity ball = ball_generator();
-    Entity paddle = paddle_generator();
+    Entity paddle1 = paddle_generator(PADDLE_LEFT);
+    Entity paddle2 = paddle_generator(PADDLE_RIGHT);
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -98,10 +113,9 @@ int main()
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        draw(ball,paddle);
-        ball_update(&ball);
-        keyboard_input(&paddle);
-
+        draw(ball,paddle1, paddle2);
+        ball_update(&ball, check_collision(&ball, &paddle1), check_collision(&ball, &paddle2));
+        keyboard_input(&paddle1, &paddle2);
 
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
@@ -115,38 +129,60 @@ int main()
 
 // ball update
 
-void ball_update( Entity *ball)
+void ball_update( Entity *ball ,bool left_paddle, bool right_paddle)
 {
     ball->x += ball->dx;
     ball->y += ball->dy;
-    if (ball->x < 0) {
+    if (ball->x < 0 && ball->dx < 0) {
         ball->dx = -ball->dx;
     }
-    if (ball->x > WINDOW_WIDTH - ball->width) {
+    if (ball->x > WINDOW_WIDTH - ball->width && ball->dx > 0) {
         ball->dx = -ball->dx;
     }
-    if (ball->y < 0) {
+    if (ball->y < 0 && ball->dy < 0) {
         ball->dy = -ball->dy;
     }
-    if (ball->y > WINDOW_HEIGHT - ball->height) {
+    if (ball->y > WINDOW_HEIGHT - ball->height && ball->dy > 0) {
         ball->dy = -ball->dy;
     }
 }
 
 //keyboard input
-void keyboard_input (Entity *paddle)
-{
-    const bool* state = SDL_GetKeyboardState(NULL);
+void keyboard_input (Entity *paddle1 , Entity *paddle2)
     {
+    const bool* state = SDL_GetKeyboardState(NULL);
         // W pressed or not
-     if (state[SDL_SCANCODE_W])
+     if (state[SDL_SCANCODE_W] && paddle1->y > 0)
      {
-         paddle->y -= paddle->dy;
+         paddle1->y -= paddle1->dy;
      }
         // S pressed or not
-        if (state[SDL_SCANCODE_S])
+        if (state[SDL_SCANCODE_S] && paddle1->y < WINDOW_HEIGHT - paddle1->height)
         {
-            paddle->y += paddle->dy;
+            paddle1->y += paddle1->dy;
+        }
+         // up arrow pressed or not
+     if (state[SDL_SCANCODE_UP] && paddle2->y > 0)
+     {
+         paddle2->y -= paddle2->dy;
+     }
+        // down arrow pressed or not
+        if (state[SDL_SCANCODE_DOWN] && paddle2->y < WINDOW_HEIGHT - paddle2->height)
+        {
+            paddle2->y += paddle2->dy;
         }
     };
-}
+
+bool check_collision(Entity *ball, Entity *paddle)
+{
+    if (ball->x < paddle->x + paddle->width &&
+        ball->x + ball->width > paddle->x &&
+        ball->y < paddle->y + paddle->height &&
+        ball->y + ball->height > paddle->y)
+    {
+        ball->dx = -ball->dx;
+        return true;
+    }else {
+        return false;
+    }
+};
